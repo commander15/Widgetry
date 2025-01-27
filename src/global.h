9a0beff188cab7e0671525cd32b255ef8e603990
commander15 <amadoubenjamain@gmail.com>
2025-01-27 17:17:50 +0100

    Added global header

---
+++ src/global.h
@@ -0,0 +1,20 @@
+#ifndef WIDGETRY_GLOBAL_H
+#define WIDGETRY_GLOBAL_H
+
+#include <Widgetry/config.h>
+
+#ifdef WIDGETRY_SHARED
+#ifdef WIDGETRY_BUILD
+#define WIDGETRY_EXPORT Q_DECL_EXPORT
+#else
+#define WIDGETRY_EXPORT Q_DECL_IMPORT
+#endif
+#else
+#define WIDGETRY_EXPORT
+#endif
+
+#define WIDGETRY_Q(ClassName) ClassName *q = static_cast<ClassName *>(q_ptr)
+#define WIDGETRY_D(ClassName) \
+    ClassName##Private *d = static_cast<ClassName##Private *>(qGetPtrHelper(d_ptr))
+
+#endif // WIDGETRY_GLOBAL_H
