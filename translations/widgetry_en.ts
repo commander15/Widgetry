922846cbc76eccd9826852d824b91811160605fa
commander15 <amadoubenjamain@gmail.com>
2025-03-24 11:35:00 +0100

    Updated translations

---
+++ translations/widgetry_en.ts
@@ -0,0 +1,170 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" sourcelanguage="en">
+<context>
+    <name>Widgetry::DataInterface</name>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.ui" line="68"/>
+        <source>Filter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.ui" line="121"/>
+        <source>Search...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.ui" line="246"/>
+        <source>N / A</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="56"/>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="485"/>
+        <source>Error during data download !</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="58"/>
+        <source>Operation failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="58"/>
+        <source>Do you want to retry ?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="70"/>
+        <source>Error during data saving !</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="222"/>
+        <source>Deletion...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="222"/>
+        <source>Do you really want to delete these %n record(s)</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="232"/>
+        <source>Error during deletion !</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="253"/>
+        <source>Show</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="256"/>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="259"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="459"/>
+        <source>Too many windows open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="473"/>
+        <source>Message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="473"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Widgetry::DataInterfaceForge</name>
+    <message>
+        <location filename="../src/lib/datainterface/datainterfaceforge.cpp" line="265"/>
+        <source>Message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterfaceforge.cpp" line="265"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Widgetry::DataSelector</name>
+    <message>
+        <location filename="../src/lib/input/dataselector.ui" line="14"/>
+        <source>Selector</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Widgetry::DataSelectorWidget</name>
+    <message>
+        <location filename="../src/lib/input/dataselectorwidget.ui" line="14"/>
+        <source>Selector</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/input/dataselectorwidget.ui" line="22"/>
+        <source>Search...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/input/dataselectorwidget.ui" line="52"/>
+        <source>Go to search bar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/input/dataselectorwidget.ui" line="63"/>
+        <source>Trigger Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Widgetry::LoginDialog</name>
+    <message>
+        <location filename="../src/lib/authentication/logindialog.ui" line="26"/>
+        <location filename="../src/lib/authentication/logindialog.cpp" line="79"/>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/authentication/logindialog.ui" line="104"/>
+        <location filename="../src/lib/authentication/logindialog.cpp" line="87"/>
+        <source>Identifier</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/authentication/logindialog.ui" line="125"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/authentication/logindialog.ui" line="167"/>
+        <source>Log In</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/authentication/logindialog.ui" line="197"/>
+        <source>²</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/authentication/logindialog.cpp" line="83"/>
+        <source>Email</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/widgetry_fr.ts
@@ -0,0 +1,170 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR" sourcelanguage="en_US">
+<context>
+    <name>Widgetry::DataInterface</name>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.ui" line="68"/>
+        <source>Filter</source>
+        <translation>Filtrer</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.ui" line="121"/>
+        <source>Search...</source>
+        <translation>Rechercher...</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.ui" line="246"/>
+        <source>N / A</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="56"/>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="485"/>
+        <source>Error during data download !</source>
+        <translation>Erreur lors du téléchargement des données !</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="58"/>
+        <source>Operation failed</source>
+        <translation>Echec de l&apos;opération</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="58"/>
+        <source>Do you want to retry ?</source>
+        <translation>Voullez vous réessayez ?</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="70"/>
+        <source>Error during data saving !</source>
+        <translation>Erreur lors de l&apos;enregistrement des données !</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="222"/>
+        <source>Deletion...</source>
+        <translation>Suppression...</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="222"/>
+        <source>Do you really want to delete these %n record(s)</source>
+        <translation>
+            <numerusform>Voullez vous supprimer cette enregistrement ?</numerusform>
+            <numerusform>Voullez vous supprimer ces %n enregistrements ?</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="232"/>
+        <source>Error during deletion !</source>
+        <translation>Erreur lors de la suppression !</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="253"/>
+        <source>Show</source>
+        <translation>Voir</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="256"/>
+        <source>Edit</source>
+        <translation>Modiffier</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="259"/>
+        <source>Delete</source>
+        <translation>Supprimer</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="459"/>
+        <source>Too many windows open</source>
+        <translation>Trop de fenêtres ouvertes</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="473"/>
+        <source>Message</source>
+        <translation>Message</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterface.cpp" line="473"/>
+        <source>Error</source>
+        <translation>Erreur</translation>
+    </message>
+</context>
+<context>
+    <name>Widgetry::DataInterfaceForge</name>
+    <message>
+        <location filename="../src/lib/datainterface/datainterfaceforge.cpp" line="265"/>
+        <source>Message</source>
+        <translation>Message</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/datainterface/datainterfaceforge.cpp" line="265"/>
+        <source>Error</source>
+        <translation>Erreur</translation>
+    </message>
+</context>
+<context>
+    <name>Widgetry::DataSelector</name>
+    <message>
+        <location filename="../src/lib/input/dataselector.ui" line="14"/>
+        <source>Selector</source>
+        <translation>Selecteur</translation>
+    </message>
+</context>
+<context>
+    <name>Widgetry::DataSelectorWidget</name>
+    <message>
+        <location filename="../src/lib/input/dataselectorwidget.ui" line="14"/>
+        <source>Selector</source>
+        <translation>Selecteur</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/input/dataselectorwidget.ui" line="22"/>
+        <source>Search...</source>
+        <translation>Rechercher...</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/input/dataselectorwidget.ui" line="52"/>
+        <source>Go to search bar</source>
+        <translation>Allez à la barre de recherche</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/input/dataselectorwidget.ui" line="63"/>
+        <source>Trigger Search</source>
+        <translation>Lancer la recherche</translation>
+    </message>
+</context>
+<context>
+    <name>Widgetry::LoginDialog</name>
+    <message>
+        <location filename="../src/lib/authentication/logindialog.ui" line="26"/>
+        <location filename="../src/lib/authentication/logindialog.cpp" line="79"/>
+        <source>Login</source>
+        <translation>Authentification</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/authentication/logindialog.ui" line="104"/>
+        <location filename="../src/lib/authentication/logindialog.cpp" line="87"/>
+        <source>Identifier</source>
+        <translation>Identifiant</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/authentication/logindialog.ui" line="125"/>
+        <source>Password</source>
+        <translation>Mot de passe</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/authentication/logindialog.ui" line="167"/>
+        <source>Log In</source>
+        <translation>Se connecter</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/authentication/logindialog.ui" line="197"/>
+        <source>²</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/authentication/logindialog.cpp" line="83"/>
+        <source>Email</source>
+        <translation>Email</translation>
+    </message>
+</context>
+</TS>
