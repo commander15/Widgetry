e4f69f8d00e4705abb64bb27e6ee48022d976a17
commander15 <amadoubenjamain@gmail.com>
2025-03-24 13:43:13 +0100

    Fixed: template related bug

--- src/lib/input/dataeditfactory.h
+++ src/lib/input/dataeditfactory.h
@@ -1,13 +1,12 @@
 #ifndef WIDGETRY_DATAEDITFACTORY_H
 #define WIDGETRY_DATAEDITFACTORY_H
 
-#include <Widgetry/global.h>
 #include <Widgetry/abstractdataeditfactory.h>
 
 namespace Widgetry {
 
 template<typename Edit>
-class WIDGETRY_EXPORT DataEditFactory : public AbstractDataEditFactory
+class DataEditFactory : public AbstractDataEditFactory
 {
 public:
     DataEditFactory<Edit> *allowDialogCreation() { enableDialogCreation(); return this; }
@@ -18,8 +17,7 @@ public:
     DataEditFactory<Edit> *maxCount(int count) { setMaxCount(count); return this; }
 
 private:
-    AbstractDataEdit *createEdit(QWidget *parent) override
-    { return new Edit(parent); }
+    AbstractDataEdit *createEdit(QWidget *parent) override { return new Edit(parent); }
 };
 
 }
